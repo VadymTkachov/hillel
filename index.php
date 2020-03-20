62004d4531599cf32ccf6bb83b04cb1726a3f89e
Tkachov Vadim Borisovich <tkachov.vadim@gmail.com>
2020-03-20 08:53:40 +0200

    HL: Create start document

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task 1</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <h1>Task #1</h1>
+    <button type="button" class="btn btn-success">Success</button>
+</div>
+</body>
+</html>
