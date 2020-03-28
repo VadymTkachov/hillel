c128e8a0adaabd70a80a96e453cfea8ee0f16437
Tkachov Vadim Borisovich <tkachov.vadim@gmail.com>
2020-03-28 22:06:36 +0200

    hl-9: Implement the generator function

--- index.php
+++ index.php
@@ -3,3 +3,22 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+function gGetFile($file)
+{
+    $f = fopen($file, 'r');
+
+    if (empty($f)) {
+        throw new Exception('File is not exists!');
+    }
+
+    while ($line = fgets($f)) {
+        yield $line;
+    }
+
+    fclose($f);
+}
+
+foreach (gGetFile(__DIR__ . '/cats.csv') as $line) {
+    echo "<pre>" . print_r(explode(',', trim($line)), true) . "</pre>";
+}
